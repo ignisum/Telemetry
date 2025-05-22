5a41ff216f740e6231d31199684255c6f57d6521
Vasilisa <logossofia56@gmail.com>
2025-05-22 21:42:31 +0300

    .

--- Main/config.py
+++
@@ -1,8 +0,0 @@
-class Config:
-    SERVER_URL = "http://localhost:15233"
-    DB_CONFIG = {
-        "dbname": "Telemetry",
-        "user": "postgres",
-        "password": "2006",
-        "host": "localhost"
-    }
